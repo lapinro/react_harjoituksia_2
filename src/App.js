9f94620ae51a21de60d4480b0f5ac353019c3308
lapinro <roosa.lapintaival@kotinet.com>
2022-08-03 12:51:36 +0300

    mooc 1.6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Palautetta</h3>
+      <Button set={setGood} fb={good} text={"hyvä"}/>
+      <Button set={setNeutral} fb={neutral} text={"neutraali"}/>
+      <Button set={setBad} fb={bad} text={"huono"}/>
+
+      <h3>Statistiikka</h3>
+      <Display feedback={"hyvä"} count={good}/>
+      <Display feedback={"neutraali"} count={neutral}/>
+      <Display feedback={"huono"} count={bad}/>
     </div>
-  );
+  )
 }
 
-export default App;
+const Button = (props) => {
+  
+  return (
+    <button onClick={() => props.set(props.fb +1)}>{props.text}</button>
+  )
+}
+
+const Display = (props) => {
+  return (
+    <div>{props.feedback} {props.count}</div>
+  )
+}
+
+export default App
